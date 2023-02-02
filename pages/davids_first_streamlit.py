b0caec333602893404b5ca8131bd5328444871bb
soccermatics <djtsumpter@me.com>
2023-02-02 16:39:05 +0100

    Update davids_first_streamlit.py

--- pages/davids_first_streamlit.py
+++ pages/davids_first_streamlit.py
@@ -7,7 +7,7 @@ import data.twelve_api as twelve
 import pandas as pd
 from settings import create_default_configs, HOME_TEAM_COLOR, AWAY_TEAM_COLOR
 from visuals.match_visuals import XGMatchTrend, XGMatchShots, XGMatchProb, PitchMatchShotmapViz, PitchHorizontalArrows
-
+from visuals.twelve_visuals import TwelvePitchVisual
 
 def sidebar_select_competition():
     # Single selector for competition
@@ -17,20 +17,10 @@ def sidebar_select_competition():
     return  competitions, selected_competition_id
 
 
-def get_match_data(match_id):
-
-    shots = twelve.get_match_shots(match_id)
-
-    stories = dict()
-    stories['stories'] = twelve.get_match_points_stories(match_id)
-
-    return stories, shots
-
-
 # Use default settings
 create_default_configs()
 
-st.header("Plotting Passes Demo")
+st.header("David plots passes")
 
 # Sidebar menu
 competitions, selected_competition_id = sidebar_select_competition()
@@ -41,13 +31,8 @@ matches = twelve.app_get_matches(selected_competition_id)
 # Create Dictionary of matches
 matches_dict = {x['matchId']: f"{x['homeTeam']['name']} {x['homeTeam']['score']}-{x['awayTeam']['score']} {x['awayTeam']['name']}" for x in matches}
 
-
 # select match
 selected_match_id = st.sidebar.selectbox("Match", matches_dict, format_func=lambda x:matches_dict[x])
-
-
-# TABS
-# tab_visuals, tab_summary, tab_shots, tab_passes = st.tabs(['Visuals', 'Summary', 'Shots', 'Passes'])
 
 # Buttons
 selected_sub_nav = st_row_buttons(['Passes'])
@@ -63,20 +48,15 @@ if  selected_sub_nav == 'Passes':
     df_passes = df_passes[df_passes['type'].isin(['goalkick'])] #throw-in,pass
     # df = df[df['points'] >= 0]
 
-    viz = PitchHorizontalArrows('AllPlayerRuns',
-                                title=f"Passes",
-                                subtitle="Goalkicks",
-                                provider='opta')
+    viz = TwelvePitchVisual("Match passes", "pass types")
 
     viz.info_text = f"Info text"
 
-    viz.set_data(df_passes['startX'],
+    fig, ax = viz.create_pass_visual(df_passes['startX'],
                  df_passes['startY'],
                  df_passes['endX'],
                  df_passes['endY'],
                  df_passes['points'])
-
-    fig, ax = viz.create_visual(False)
 
     st.columns(3)[0].pyplot(fig, dpi=100, facecolor=fig.get_facecolor(), bbox_inches=None)
 
